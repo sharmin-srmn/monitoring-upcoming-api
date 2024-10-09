5323bd72f83aa0a0d66734c68962952cfd72ba29
Sharmin Akhter <sharmin.srmn@gmail.com>
2024-10-09 12:43:54 +0600

    Minor changes

--- monitoringAPI/lib/data.js
+++ monitoringAPI/lib/data.js
@@ -8,14 +8,14 @@ lib.baseDir = path.join(__dirname,'/../.data/');
 
 lib.createData = (dir, fileName, data, callback) =>{
     //FOR CREATING DATA FIRST WE NEED TO OPEN FILE
-    fs.open(`${lib.baseDir}${dir}/${fileName}.json`, 'wx',  (error1, fileDescriptor) =>{
-        if(!error1 && fileDescriptor){
+    fs.open(`${lib.baseDir}${dir}/${fileName}.json`, 'wx',  (errorOpening, fileDescriptor) =>{
+        if(!errorOpening && fileDescriptor){
             //
             const dataString = JSON.stringify(data);
-            fs.writeFile(fileDescriptor, dataString, (error2) => {
-                if(!error2){
-                    fs.close(fileDescriptor, (error3) =>{
-                        if(!error3){
+            fs.writeFile(fileDescriptor, dataString, (errorWriting) => {
+                if(!errorWriting){
+                    fs.close(fileDescriptor, (errorClosing) =>{
+                        if(!errorClosing){
                             callback(false);
                         }else{
                             callback('Error Closing file');
@@ -26,29 +26,29 @@ lib.createData = (dir, fileName, data, callback) =>{
                 }
             });
         }else{
-            callback('Error opening file.File may already exists.');
+            callback('Error opening file.File may already exists.Cant register with same number.');
         }
     });
 }
 lib.readData = (dir, fileName, callback ) =>{
-    fs.readFile(`${lib.baseDir}${dir}/${fileName}.json`, 'utf-8', (error, data) =>{
-        if(!error){
-            callback(error, data);
+    fs.readFile(`${lib.baseDir}${dir}/${fileName}.json`, 'utf-8', (errorReading, data) =>{
+        if(!errorReading){
+            callback(errorReading, data);
         }else{
             callback('Error reading File.')
         }
     });
 }
 lib.updateData = (dir, fileName, data, callback) =>{
-    fs.open(`${lib.baseDir}${dir}/${fileName}.json`, 'r+', (error1, fileDescriptor) =>{
-        if(!error1 && fileDescriptor){
+    fs.open(`${lib.baseDir}${dir}/${fileName}.json`, 'r+', (errorOpening, fileDescriptor) =>{
+        if(!errorOpening && fileDescriptor){
             const dataString = JSON.stringify(data);
-            fs.ftruncate(fileDescriptor, (error2)=>{
-                if(!error2){
-                    fs.writeFile(fileDescriptor, dataString, (error3) =>{
-                        if(!error3){
-                            fs.close(fileDescriptor, (error4) =>{
-                                if(!error4){
+            fs.ftruncate(fileDescriptor, (errorTruncating)=>{
+                if(!errorTruncating){
+                    fs.writeFile(fileDescriptor, dataString, (errorWriting) =>{
+                        if(!errorWriting){
+                            fs.close(fileDescriptor, (errorClosing) =>{
+                                if(!errorClosing){
                                     callback(false)
                                 }else{
                                     callback("Error Closing file.");
@@ -69,8 +69,8 @@ lib.updateData = (dir, fileName, data, callback) =>{
     });
 }
 lib.deleteData = (dir, fileName, callback) =>{
-    fs.unlink(`${lib.baseDir}${dir}/${fileName}.json`, (error)=>{
-        if(!error){
+    fs.unlink(`${lib.baseDir}${dir}/${fileName}.json`, (errorDeleting)=>{
+        if(!errorDeleting){
             callback(false);
         }else{
             callback('Error Deleting File');
